2a9502972bb1bd58e5f7f413d86887bd2ec6326b
AmilaMirvic <58607566+AmilaMirvic@users.noreply.github.com>
2021-04-01 17:42:13 +0200

    AngularJS PhoneBook

--- src/app/phonebook/phonebook.component.ts
+++ src/app/phonebook/phonebook.component.ts
@@ -7,15 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhonebookComponent implements OnInit {
   phonebook: Array<object>;
-  contact: {name:string, phone: string};
+  contact: { name: string, phone: string };
+  name: string;
+  phone: string;
 
   constructor() {
     this.phonebook = [];
-   }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.loadFromLocalStorage();
+  }
+
+  saveContact() {
+    this.phonebook.push({ name: this.name, phone: this.phone });
+    this.saveToLocalStorage();
+    this.phone = "";
+    this.name = "";
+  };
+
+  saveToLocalStorage() {
+    localStorage.setItem('phonebook', JSON.stringify(this.phonebook));
+  }
+
+  loadFromLocalStorage() {
+    this.phonebook = JSON.parse(localStorage.getItem("phonebook")) || [];
+  }
+
+  deleteFromLocalStorage(ev) {
+    let index = this.phonebook.indexOf(ev)
+    this.phonebook.splice(index, 1);
+    this.saveToLocalStorage()
+    this.loadFromLocalStorage();
+  }
 
 
 }
